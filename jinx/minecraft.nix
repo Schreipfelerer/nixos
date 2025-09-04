8c80f844fb6ba4a34735cc8a8b23f05f1594ecc8
Thabo <Schreipfelerer@gmail.com>
2025-09-04 13:15:55 +0200

    minecraft

--- jinx/minecraft.nix
+++ jinx/minecraft.nix
@@ -5,26 +5,29 @@
         image = "itzg/minecraft-server:java17";
         ports = ["25565:25565"];
         volumes = [
-            "var/lib/minecraft/startechnology/data/:/data/"
+            "/var/lib/minecraft/startechnology/data/:/data/"
         ];
-        enviroment = {
+        environment = {
             EULA = "TRUE";
             INIT_MEMORY = "4G";
             MAX_MEMORY = "8G";
+	    TYPE = "AUTO_CURSEFORGE";
             TZ = "CEST";
             MOTD = "Star";
+	    LEVEL_TYPE = "skyblockbuilder:skyblock";
+	    ALLOW_FLIGHT = "true";
             ENABLE_WHITELIST = "true";
             Whitelist = "Schreipfelerer";
-            CF_API_KEY = "";
+	    OPS = "Schreipfelerer";
             CF_SLUG = "star-technology";
         };
         environmentFiles = [
           config.sops.secrets."minecraft/CF_API_KEY".path
         ];
-        extraOptions = [
-            "--tty"
-            "--interactive"
-        ];
+        # extraOptions = [
+        #     "--tty"
+        #     "--interactive"
+        # ];
         autoStart = false;
     };
 }
