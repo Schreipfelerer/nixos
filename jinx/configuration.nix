a6d67732b55fb8a76394266c366bd9e1aeb0e52f
Thabo <Schreipfelerer@gmail.com>
2025-11-27 15:24:08 +0100

    neovim

--- jinx/configuration.nix
+++ jinx/configuration.nix
@@ -84,13 +84,18 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      git
      fastfetch
      sops
      btop
      dig
   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+  };
   
   programs.nh = {
     enable = true;
