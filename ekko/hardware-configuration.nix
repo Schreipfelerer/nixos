64bddb91c4f7c1bc3679ef0879511f9ea9a29dee
Schreipfelerer <Schreipfelerer@gmail.com>
2026-03-06 23:14:17 +0800

    upadte hardware

--- ekko/hardware-configuration.nix
+++ ekko/hardware-configuration.nix
@@ -14,35 +14,35 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b0bde4c4-267a-43c1-a63a-f30bf37129a0";
+    { device = "/dev/mapper/root";
       fsType = "btrfs";
       options = [ "subvol=@" ];
     };
 
   boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/8edadcc3-8f41-4285-8f8d-9bea7e921d8f";
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/b0bde4c4-267a-43c1-a63a-f30bf37129a0";
-      fsType = "btrfs";
-      options = [ "subvol=@home" ];
-    };
-
   fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/b0bde4c4-267a-43c1-a63a-f30bf37129a0";
+    { device = "/dev/mapper/root";
       fsType = "btrfs";
       options = [ "subvol=@var_log" ];
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/b0bde4c4-267a-43c1-a63a-f30bf37129a0";
+    { device = "/dev/mapper/root";
       fsType = "btrfs";
       options = [ "subvol=@nix" ];
     };
 
   fileSystems."/swap" =
-    { device = "/dev/disk/by-uuid/b0bde4c4-267a-43c1-a63a-f30bf37129a0";
+    { device = "/dev/mapper/root";
       fsType = "btrfs";
       options = [ "subvol=@swap" ];
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/mapper/root";
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
     };
 
   fileSystems."/boot" =
@@ -52,13 +52,6 @@
     };
 
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp170s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
