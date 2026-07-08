39833ecfbd8833c75cc4c158828cb1bd1ed99c2f
Thabo <Schreipfelerer@gmail.com>
2026-07-08 19:42:13 +0200

    atm10

--- jinx/minecraft.nix
+++ jinx/minecraft.nix
@@ -30,4 +30,33 @@
     # ];
     autoStart = false;
   };
+
+  virtualisation.oci-containers.containers."mc-atm10" = {
+    image = "itzg/minecraft-server:java21";
+    ports = [ "25565:25565" ];
+    volumes = [
+      "/var/lib/minecraft/atm10/data/:/data/"
+    ];
+    environment = {
+      EULA = "TRUE";
+      INIT_MEMORY = "4G";
+      MAX_MEMORY = "8G";
+      TYPE = "AUTO_CURSEFORGE";
+      TZ = "CEST";
+      MOTD = "All the Mods?";
+      ALLOW_FLIGHT = "true";
+      ENABLE_WHITELIST = "true";
+      Whitelist = "Schreipfelerer";
+      OPS = "Schreipfelerer";
+      CF_SLUG = "all-the-mods-10";
+    };
+    environmentFiles = [
+      config.sops.secrets."minecraft/CF_API_KEY".path
+    ];
+    # extraOptions = [
+    #     "--tty"
+    #     "--interactive"
+    # ];
+    autoStart = true;
+  };
 }
