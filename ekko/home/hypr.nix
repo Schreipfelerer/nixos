856fe9bdcbdb3ef4bba38c162c7ce60122850ac0
Schreipfelerer <Schreipfelerer@gmail.com>
2026-07-24 15:11:47 +0200

    fixed hyprpaper

--- ekko/home/hypr.nix
+++ ekko/home/hypr.nix
@@ -12,8 +12,12 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = "/home/bo/git/wallpapers/gruvbox-nix.png";
-      wallpaper = "eDP-1, /home/bo/git/wallpapers/gruvbox-nix.png";
+      wallpaper = [
+        {
+          monitor = "eDP-1";
+          path = "/home/bo/git/wallpapers/gruvbox-nix.png";
+        }
+      ];
     };
   };
 
